147aa91665ff461f29a497ff2e03526c1862551d
edhz8888 <edhz8888@gmail.com>
2021-02-22 11:05:45 +0900

    customed to use with Nuxt
    customed to use with Nuxt

--- team-blue/build/page.js
+++ team-blue/build/page.js
@@ -50,43 +50,45 @@
 
   class BlueBuy extends HTMLElement {
     static get observedAttributes() {
-      return ['sku'];
+      return ["sku"];
     }
 
     connectedCallback() {
       this.addToCart = this.addToCart.bind(this);
-      const sku = this.getAttribute('sku');
-      this.log('connected', sku);
+      const sku = this.getAttribute("sku");
+      this.log("connected", sku);
       this.render();
-      this.firstChild.addEventListener('click', this.addToCart);
+      this.addEventListener("click", this.addToCart);
     }
 
     addToCart() {
       window.blue.count += 1;
       this.log('event sent "blue:basket:changed"');
-      this.dispatchEvent(new CustomEvent('blue:basket:changed', {
-        bubbles: true,
-      }));
+      this.dispatchEvent(
+        new CustomEvent("blue:basket:changed", {
+          bubbles: true,
+        })
+      );
     }
 
     render() {
-      const sku = this.getAttribute('sku');
+      const sku = this.getAttribute("sku");
       this.innerHTML = renderBuy(sku);
     }
 
     attributeChangedCallback(attr, oldValue, newValue) {
-      this.log('attributeChanged', attr, oldValue, newValue);
+      this.log("attributeChanged", attr, oldValue, newValue);
       this.render();
     }
 
     disconnectedCallback() {
-      this.firstChild.removeEventListener('click', this.addToCart);
-      const sku = this.getAttribute('sku');
-      this.log('disconnected', sku);
+      this.removeEventListener("click", this.addToCart);
+      const sku = this.getAttribute("sku");
+      this.log("disconnected", sku);
     }
 
     log(...args) {
-      console.log('🔘 blue-buy', ...args);
+      console.log("🔘 blue-buy", ...args);
     }
   }
 
--- team-blue/src/blue-buy/custom-element.js
+++ team-blue/src/blue-buy/custom-element.js
@@ -1,46 +1,48 @@
 /* eslint-disable no-use-before-define, no-console, class-methods-use-this */
 /* globals HTMLElement, window, CustomEvent */
-import render from './render';
+import render from "./render";
 
 class BlueBuy extends HTMLElement {
   static get observedAttributes() {
-    return ['sku'];
+    return ["sku"];
   }
 
   connectedCallback() {
     this.addToCart = this.addToCart.bind(this);
-    const sku = this.getAttribute('sku');
-    this.log('connected', sku);
+    const sku = this.getAttribute("sku");
+    this.log("connected", sku);
     this.render();
-    this.firstChild.addEventListener('click', this.addToCart);
+    this.addEventListener("click", this.addToCart);
   }
 
   addToCart() {
     window.blue.count += 1;
     this.log('event sent "blue:basket:changed"');
-    this.dispatchEvent(new CustomEvent('blue:basket:changed', {
-      bubbles: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent("blue:basket:changed", {
+        bubbles: true,
+      })
+    );
   }
 
   render() {
-    const sku = this.getAttribute('sku');
+    const sku = this.getAttribute("sku");
     this.innerHTML = render(sku);
   }
 
   attributeChangedCallback(attr, oldValue, newValue) {
-    this.log('attributeChanged', attr, oldValue, newValue);
+    this.log("attributeChanged", attr, oldValue, newValue);
     this.render();
   }
 
   disconnectedCallback() {
-    this.firstChild.removeEventListener('click', this.addToCart);
-    const sku = this.getAttribute('sku');
-    this.log('disconnected', sku);
+    this.removeEventListener("click", this.addToCart);
+    const sku = this.getAttribute("sku");
+    this.log("disconnected", sku);
   }
 
   log(...args) {
-    console.log('🔘 blue-buy', ...args);
+    console.log("🔘 blue-buy", ...args);
   }
 }
 
